df3683ab9061ccffe730ca8c7d1813468f98049d
Yuxiang <Yuxiang.L.Qin@gmail.com>
2018-05-04 00:01:44 +1000

    update available_redshifts and plots.ipynb

--- astrodatapy/misc_tools.py
+++ astrodatapy/misc_tools.py
@@ -8,11 +8,18 @@ def filename_to_redshift(fname):
     return float(interger) + float(decimal) / 10**len(decimal)
 
 def available_redshifts(feature):
-    import glob
-    all_fnames = glob.glob('data/%s/**/*'%feature)
+    import os
+    import fnmatch
+    fileDir = os.path.dirname(os.path.abspath(__file__))
+    all_fnames = []
+    for root, dirnames, filenames in os.walk(fileDir+'/data/%s'%feature):
+        for filename in fnmatch.filter(filenames, '*'):
+            if 'dat' in filename:
+                all_fnames.append(filename)
+
+    #all_fnames = glob.glob('data/%s/**/*'%feature)
     if len(all_fnames)>0:
-        all_fnames = [f[f.rfind('z'):] for f in all_fnames]
-        results    = [filename_to_redshift(fname) for fname in set(all_fnames)]
-        return np.array(results)
+        #all_fnames = [f[f.rfind('z'):] for f in all_fnames]
+        return np.array([filename_to_redshift(fname) for fname in set(all_fnames)])
     else:
         return np.array([])
